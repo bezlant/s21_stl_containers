e4bf5e32a8fc7b04f1744aa81f4af3326bd4e04b
Anthony <89563512+bezlant@users.noreply.github.com>
2022-11-01 15:03:24 +0300

    feat(stack): Stack is fully functional & tested (Implemented using list). (#6)
    * feat: Stack is implemented relying on s21::list
    
    * feat: Stack tests implemented.
    
    * fix: Removed redundant keyword.

---
+++ src/s21_stack.hpp
@@ -0,0 +1,86 @@
+#ifndef S21_STACK_H_
+#define S21_STACK_H_
+
+#include <initializer_list>
+#include <algorithm>
+// HACK: Replace this with s21::list<T>
+#include <list>
+
+namespace s21 {
+
+// HACK: Replace this with s21::list<T>
+template <typename T, typename Container = std::list<T>>
+class Stack {
+  public:
+    using value_type = T;
+    using reference = T &;
+    using const_reference = const T &;
+    using size_type = std::size_t;
+
+    // Member functions
+  public:
+    Stack() noexcept : s{} {
+    }
+
+    // Constructors below may look weird but, they will call the required
+    // constructor of the Container, assuming the Container implements it.
+    explicit Stack(std::initializer_list<value_type> const &items) : s{items} {
+    }
+
+    Stack(const Stack &rhs) : s{rhs.s} {
+    }
+
+    Stack(Stack &&rhs) noexcept : s{std::move(rhs.s)} {
+    }
+
+    ~Stack() noexcept = default;
+
+    Stack &operator=(const Stack &other) {
+        s = other.s;
+        return *this;
+    }
+
+    Stack &operator=(Stack &&other) noexcept {
+        s = std::move(other.s);
+        return *this;
+    }
+
+    // Element access
+  public:
+    reference top() noexcept {
+        return s.back();
+    }
+
+    const_reference top() const noexcept {
+        return s.back();
+    }
+
+    // Capacity
+  public:
+    [[nodiscard]] bool empty() const noexcept {
+        return s.empty();
+    }
+
+    size_type size() const noexcept {
+        return s.size();
+    }
+    // Stack Modifiers
+    void push(const_reference value) {
+        s.push_back(value);
+    }
+
+    void pop() noexcept {
+        s.pop_back();
+    }
+
+    void swap(Stack &other) noexcept {
+        std::swap(s, other.s);
+    }
+
+  private:
+    Container s;
+};
+
+}  // namespace s21
+
+#endif  // S21_STACK_H_
---
+++ src/tests/test_stack.cpp
@@ -0,0 +1,112 @@
+#include "../s21_stack.hpp"
+#include "gtest/gtest.h"
+
+class StackTest : public ::testing::Test {
+  protected:
+    void SetUp() override {
+    }
+
+    s21::Stack<int> s0_{1, 2, 3, 4, 5};
+    s21::Stack<int> s1_{};
+};
+
+TEST_F(StackTest, default_constructor) {
+    ASSERT_TRUE(s1_.empty());
+    ASSERT_EQ(s1_.size(), 0);
+}
+
+TEST_F(StackTest, initializer_list_constructor) {
+    ASSERT_EQ(s0_.top(), 5);
+    ASSERT_FALSE(s0_.empty());
+    ASSERT_EQ(s0_.size(), 5);
+    s0_.pop();
+    ASSERT_EQ(s0_.size(), 4);
+    s0_.pop();
+    ASSERT_EQ(s0_.size(), 3);
+    ASSERT_EQ(s0_.top(), 3);
+    s0_.pop();
+    s0_.pop();
+    s0_.pop();
+    ASSERT_TRUE(s0_.empty());
+
+    // NOTE:exception test here when s21::list added
+    // ASSERT_ANY_THROW(s0_.pop());
+}
+
+TEST_F(StackTest, copy_constructor) {
+    s21::Stack copy(s0_);
+    ASSERT_EQ(copy.top(), s0_.top());
+    copy.pop();
+    s0_.pop();
+    ASSERT_EQ(copy.top(), s0_.top());
+    copy.pop();
+    s0_.pop();
+    ASSERT_EQ(copy.top(), s0_.top());
+    copy.pop();
+    s0_.pop();
+    ASSERT_EQ(copy.top(), s0_.top());
+    copy.pop();
+    s0_.pop();
+    ASSERT_EQ(copy.top(), s0_.top());
+    copy.pop();
+    s0_.pop();
+
+    ASSERT_TRUE(copy.empty());
+    ASSERT_TRUE(s0_.empty());
+
+    copy.push(69);
+    s0_.push(69);
+    ASSERT_EQ(copy.top(), s0_.top());
+}
+
+TEST_F(StackTest, move_constructor) {
+    s21::Stack moved(std::move(s0_));
+    ASSERT_EQ(moved.size(), 5);
+    ASSERT_EQ(moved.top(), 5);
+}
+
+TEST_F(StackTest, copy_assignment) {
+    s21::Stack<int> copy;
+    copy = s0_;
+    ASSERT_EQ(copy.top(), s0_.top());
+    copy.pop();
+    s0_.pop();
+    ASSERT_EQ(copy.top(), s0_.top());
+    copy.pop();
+    s0_.pop();
+    ASSERT_EQ(copy.top(), s0_.top());
+    copy.pop();
+    s0_.pop();
+    ASSERT_EQ(copy.top(), s0_.top());
+    copy.pop();
+    s0_.pop();
+    ASSERT_EQ(copy.top(), s0_.top());
+    copy.pop();
+    s0_.pop();
+
+    ASSERT_TRUE(copy.empty());
+    ASSERT_TRUE(s0_.empty());
+
+    copy.push(69);
+    s0_.push(69);
+    ASSERT_EQ(copy.top(), s0_.top());
+}
+
+TEST_F(StackTest, move_assignment) {
+    s21::Stack<int> moved;
+    moved = std::move(s0_);
+    ASSERT_EQ(moved.size(), 5);
+    ASSERT_EQ(moved.top(), 5);
+}
+
+TEST_F(StackTest, swap) {
+    ASSERT_EQ(s1_.size(), 0);
+    ASSERT_EQ(s0_.size(), 5);
+    ASSERT_FALSE(s0_.empty());
+    ASSERT_TRUE(s1_.empty());
+    s0_.swap(s1_);
+    ASSERT_FALSE(s1_.empty());
+    ASSERT_TRUE(s0_.empty());
+    ASSERT_EQ(s0_.size(), 0);
+    ASSERT_EQ(s1_.size(), 5);
+}
